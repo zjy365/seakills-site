89733cafc8676de0b884d5ae7d56017bf7de71b2
zjy365 <3161362058@qq.com>
2026-03-31 15:12:39 +0800

    Sync skill docs and site constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const REPO = "zjy365/seakills"
-export const GITHUB_URL = "https://github.com/zjy365/seakills"
+export const REPO = "labring/seakills"
+export const GITHUB_URL = "https://github.com/labring/seakills"
 export const INSTALL_CMD = `npx skills add ${REPO}`
